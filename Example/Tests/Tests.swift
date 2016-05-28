230d7f2fc11fc254a71344e54b1ccbb33ca49a91
Craig Siemens <siemens.craig@gmail.com>
2016-05-27 20:36:50 -0600

    Fixed tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -8,28 +8,28 @@ class DividerViewSpec: QuickSpec {
     override func spec() {
         describe("horizontal divider") {
 
-//            let divider = DividerView(axis: .horizontal)
-//            
-//            it("should have a intrinsic content height of 1") {
-//                expect(divider.intrinsicContentSize().height).to(equal(1))
-//            }
-//            
-//            it("should have a intrinsic content width of UIViewNoIntrinsicMetric") {
-//                expect(divider.intrinsicContentSize().width).to(equal(UIViewNoIntrinsicMetric))
-//            }
+            let divider = DividerView(axis: .horizontal)
+            
+            it("should have a intrinsic content height of 1") {
+                expect(divider.intrinsicContentSize().height).to(equal(1))
+            }
+            
+            it("should have a intrinsic content width of UIViewNoIntrinsicMetric") {
+                expect(divider.intrinsicContentSize().width).to(equal(UIViewNoIntrinsicMetric))
+            }
         }
 
         describe("vertical divider") {
             
-//            let divider = DividerView(axis: .vertical)
-//            
-//            it("should have a intrinsic content height of UIViewNoIntrinsicMetric") {
-//                expect(divider.intrinsicContentSize().height).to(equal(UIViewNoIntrinsicMetric))
-//            }
-//            
-//            it("should have a intrinsic content width of 1") {
-//                expect(divider.intrinsicContentSize().width).to(equal(1))
-//            }
+            let divider = DividerView(axis: .vertical)
+            
+            it("should have a intrinsic content height of UIViewNoIntrinsicMetric") {
+                expect(divider.intrinsicContentSize().height).to(equal(UIViewNoIntrinsicMetric))
+            }
+            
+            it("should have a intrinsic content width of 1") {
+                expect(divider.intrinsicContentSize().width).to(equal(1))
+            }
         }
     }
 }
